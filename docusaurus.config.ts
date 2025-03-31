2b656b100c8a0bd1694d44ada99a53dc6fb6d444
nelsonksh <nelsonksh@gimbalabs.io>
2025-03-31 19:58:21 +0530

    update sdk entrypoint

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
     [
       'docusaurus-plugin-typedoc',
       {
-        entryPoints: ['node-sdk/src/index.ts', 'node-sdk/src/types/index.ts', 'node-sdk/src/indexer/core/index.ts'],
+        entryPoints: ['node-sdk/src/index.ts', 'node-sdk/src/provider/index.ts'],
         tsconfig: 'node-sdk/tsconfig.json',
         out: 'docs/sdk',
         sidebar: {
